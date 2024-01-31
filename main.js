62a4eddd886a88380a90173fe2b3da352a620a46
mikespike1985 <154455164+mikespike1985@users.noreply.github.com>
2024-01-31 00:07:40 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+
+// Create a variable to store my age
+const myAge = 38
+
+//Create a variable to store the 'early years'
+var earlyYears = 2
+
+earlyYears *= 10.5
+
+// Create a variable to store the later years
+var laterYears = myAge - 2
+
+// Multiply the lateryears variable 
+laterYears *= 4
+
+//check my code
+console.log(earlyYears)
+console.log(laterYears)
+
+//combine both sets of years
+myAgeInDogYears = earlyYears + laterYears
+
+//Create a variable to store my name
+var myName = 'Michael Douek'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
